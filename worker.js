7f5f802578c550d6b5ad87e313530e1fe37aa0a5
Jack Horgan <tsnm61tt@duck.com>
2025-12-18 23:00:57 -0500

    fix use of secrets

--- worker.js
+++ worker.js
@@ -3,7 +3,7 @@
 
 export default {
   // Main fetch handler that processes all incoming requests
-  async fetch(request) {
+  async fetch(request, env) {
     // Parse the incoming request URL to extract pathname and other components
     const url = new URL(request.url);
 
@@ -13,7 +13,7 @@ export default {
     // If the URL matches our target pattern
     if (match) {
 
-      let newUrlString = request.url.search.replace("WOA_BUDDY_THATS_NOT_AN_API_KEY", env:DEV_safe-browsing-api-key);
+      let newUrlString = request.url.search.replace("FAKE_API_KEY", env.DEV_safe-browsing-api-key);
       newUrlString = newUrlString.replace("warpstar.scamjam.app", "safebrowsing.googleapis.com");
 
       // Create a new request with the rewritten URL, preserving all original request properties
